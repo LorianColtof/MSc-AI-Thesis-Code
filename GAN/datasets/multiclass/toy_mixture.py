Fix multimarginal toy dataset variance and plot target distributions samples

GAN/datasets/multiclass/toy_mixture.py
@@ -2,7 +2,7 @@ import io
 import os
 import pickle
 from math import pi
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Tuple
 
 import seaborn
 import torch
@@ -22,7 +22,9 @@ class ToyMixtureDataset(AbstractBaseMulticlassDataset):
     _num_plot_samples = 10000
 
     _source_samples: torch.Tensor
-    _source_samples_data: bytes
+    _pickle_data: Dict[str, bytes]
+
+    _plot_colors = ['red', 'blue', 'orange', 'purple', 'brown', 'tan']
 
     def __init__(self, dataset_config: Dataset, device: torch.device,
                  num_workers: int, batch_size: int, latent_dimension: int):
@@ -32,33 +34,45 @@ class ToyMixtureDataset(AbstractBaseMulticlassDataset):
 
         def create_dataloader(x: float, y: float) -> DataLoader:
             mean = torch.tensor([x, y], device=device)
-            variance = torch.ones(2, device=device)
-            dist = D.Normal(mean, variance)
+            scale = torch.Tensor([0.2], device=device)
+            dist = D.Normal(mean, scale)
             samples = dist.sample((num_samples, ))
 
             return DataLoader(
                 TensorDataset(samples),
                 batch_size=batch_size, shuffle=True, drop_last=True)
 
+        def sample_dataloader(dataloader: DataLoader) -> Tuple[torch.Tensor, bytes]:
+            samples = next(iter(dataloader))[0][:self._num_plot_samples].cpu().detach()
+            buffer = io.BytesIO()
+            torch.save(samples, buffer)
+            samples_data = buffer.getvalue()
+            buffer.close()
+
+            return samples, samples_data
+
         self.source_dataloader = create_dataloader(0.0, 0.0)
+        self._source_samples, source_samples_data =\
+            sample_dataloader(self.source_dataloader)
+
+        self._pickle_data = {
+            'source': source_samples_data
+        }
+
         self.target_dataloaders = {}
+        self._target_samples = {}
 
         for i in range(num_mixtures):
             angle = torch.tensor([(2 * pi * i) / num_mixtures], device=device)
             x = radius * torch.cos(angle)
             y = radius * torch.sin(angle)
 
-            self.target_dataloaders[str(i)] = create_dataloader(x, y)
+            _class = str(i)
+            self.target_dataloaders[_class] = create_dataloader(x, y)
 
-        self._source_samples = \
-            next(iter(self.source_dataloader))[0][:self._num_plot_samples] \
-            .cpu().detach()
-
-        buffer = io.BytesIO()
-        torch.save(self._source_samples, buffer)
-        self._source_samples_data = buffer.getvalue()
-
-        buffer.close()
+            samples, samples_data = sample_dataloader(self.target_dataloaders[_class])
+            self._target_samples[_class] = samples
+            self._pickle_data[f'{_class}_real'] = samples_data
 
     def save_generated_data(self, source_data: Tensor,
                             generated_data: Dict[str, Tensor], images_path: str,
@@ -70,18 +84,20 @@ class ToyMixtureDataset(AbstractBaseMulticlassDataset):
         plt.plot(self._source_samples[:, 0], self._source_samples[:, 1],
                  'o', alpha=0.2, color='g')
 
-        pickle_data = {
-            'source': self._source_samples_data
-        }
+        for i, (_class, data) in enumerate(generated_data.items()):
+            target_samples = \
+                next(iter(self.target_dataloaders[_class]))[0][:self._num_plot_samples]\
+                .cpu().detach()
+            plt.plot(target_samples[:, 0], target_samples[:, 1],
+                     'o', alpha=0.2, color=self._plot_colors[i])
 
-        for _class, data in generated_data.items():
             data_cpu = data.cpu().detach()
             seaborn.kdeplot(x=data_cpu[:, 0], y=data_cpu[:, 1], zorder=0,
-                            n_levels=5, shade=True)
+                            n_levels=5, shade=True, color=self._plot_colors[i])
 
             buffer = io.BytesIO()
             torch.save(data_cpu, buffer)
-            pickle_data[_class] = buffer.getvalue()
+            self._pickle_data[f'{_class}_generated'] = buffer.getvalue()
             buffer.close()
 
         img_path = os.path.join(images_path, f'{filename}.pdf')
@@ -89,7 +105,7 @@ class ToyMixtureDataset(AbstractBaseMulticlassDataset):
 
         data_path = os.path.join(images_path, f'{filename}.pkl')
         with open(data_path, 'wb') as f:
-            pickle.dump(pickle_data, f, protocol=DEFAULT_PROTOCOL)
+            pickle.dump(self._pickle_data, f, protocol=DEFAULT_PROTOCOL)
 
         return [img_path, data_path]
 